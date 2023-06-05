13cded4eeef334c76a30d3d63d2c574c66a55097
flsouto <fabiolimasouto@gmail.com>
2023-06-05 18:37:30 -0300

    updates

--- cleanup.php
+++ cleanup.php
@@ -9,7 +9,7 @@ $exceeds = $count - 1000;
 if($exceeds > 0){
     $rm_amount = min($exceeds, $amount);
     echo "Removing $rm_amount\n";
-    $list = remote_exec("cd loops; ls *.mp3 -t | tail -n $rm_amount");
-    print_r($list);
+    $list = remote_exec("cd loops; ls *.mp3 -t | tail -n $rm_amount > /dev/shm/remove; rm $(cat /dev/shm/remove); cat /dev/shm/remove;");
+    echo $list;
 }
 
